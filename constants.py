908f7a65bcd33ababda546c7edbf22a250d8d4cd
Prateek Thakur <prateekthakur272@gmail.com>
2023-08-21 23:21:45 +0530

    fetches weather

---
+++ constants.py
@@ -0,0 +1,1 @@
+api_key = 'c1b0b3efded3f426f2cb9f1806199cd6'
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import json
+import urllib.request
+from constants import api_key
+
 
 # Create your views here.
+def index(req):
+    data = {}
+    if req.method == 'POST':
+        city = req.POST['city']
+        res = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}').read()
+        res_data = json.loads(res)
+        data = {
+            'country_code': str(res_data['sys']['country']),
+            'coordinate': f"{res_data['coord']['lon']} {res_data['coord']['lat']}",
+            'temp': f"{res_data['main']['temp']}k",
+            'pressure': str(res_data['main']['pressure']),
+            'humidity': str(res_data['main']['humidity']),
+        }
+        print(data)
+    return render(req, 'index.html', data)
