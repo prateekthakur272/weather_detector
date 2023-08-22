80fa33867efa3c0650576ab4e2fb08ac21d5063a
Prateek Thakur <prateekthakur272@gmail.com>
2023-08-23 00:16:07 +0530

    error

--- weather/views.py
+++ weather/views.py
@@ -8,15 +8,19 @@ from constants import api_key
 def index(req):
     data = {}
     if req.method == 'POST':
-        city = req.POST['city']
-        res = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}').read()
-        res_data = json.loads(res)
-        data = {
-            'country_code': str(res_data['sys']['country']),
-            'coordinate': f"{res_data['coord']['lon']} {res_data['coord']['lat']}",
-            'temp': f"{res_data['main']['temp']}k",
-            'pressure': str(res_data['main']['pressure']),
-            'humidity': str(res_data['main']['humidity']),
-        }
+        try:
+            city = req.POST['city']
+            res = urllib.request.urlopen(
+                f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}').read()
+            res_data = json.loads(res)
+            data = {
+                'country_code': str(res_data['sys']['country']),
+                'coordinate': f"{res_data['coord']['lon']} {res_data['coord']['lat']}",
+                'temp': f"{res_data['main']['temp']}k",
+                'pressure': str(res_data['main']['pressure']),
+                'humidity': str(res_data['main']['humidity']),
+            }
+        except:
+            return render(req, 'error.html')
         return render(req, 'index.html', {'data': data, 'city': city})
     return render(req, 'index.html')
